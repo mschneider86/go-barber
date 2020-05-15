b35eab5a6a1aad0b5b065cbc3727e1de52166cf2
Matheus Schneider <matheus.ssm@gmail.com>
2020-05-15 18:41:44 -0300

    Created user update feature and unit test

---
+++ gobarber-backend/src/modules/users/services/UpdateProfileService.ts
@@ -0,0 +1,66 @@
+import path from 'path';
+import fs from 'fs';
+import AppError from '@shared/errors/AppError';
+import uploadConfig from '@config/upload';
+import { injectable, inject } from 'tsyringe';
+
+import User from '../infra/typeorm/entities/User';
+import IUsersRepository from '../repositories/IUsersRepository';
+import IHashProvider from '../providers/HashProvider/models/IHashProvider';
+
+interface Request {
+  user_id: string;
+  name: string;
+  email: string;
+  old_password?: string;
+  password?: string;
+}
+
+@injectable()
+class UpdateProfile {
+  constructor(
+    @inject('UsersRepository')
+    private usersRepository: IUsersRepository,
+
+    @inject('HashProvider')
+    private hashProvider: IHashProvider,
+  ) { }
+
+  public async execute({ user_id, name, email, password, old_password }: Request): Promise<User> {
+    const user = await this.usersRepository.findById(user_id);
+
+    if(!user) {
+      throw new AppError('User not found.');
+    }
+
+    const userWithUpdatedEmail = await this.usersRepository.findByEmail(email);
+
+    if (userWithUpdatedEmail && userWithUpdatedEmail.id !== user_id) {
+      throw new AppError('E-mail already in use.');
+    }
+
+    user.name = name;
+    user.email = email;
+
+    if (password && !old_password) {
+      throw new AppError('You must inform your current password to set a new one.');
+    }
+
+    if (password && old_password) {
+      const checkOldPassword = await this.hashProvider.compareHash(
+        old_password,
+        user.password,
+      );
+
+      if(!checkOldPassword) {
+        throw new AppError('Current password does not match.');
+      }
+
+      user.password = await this.hashProvider.generateHash(password);
+    }
+
+    return this.usersRepository.save(user);
+  }
+}
+
+export default UpdateProfile;
